ffd3334acf515abfbfd9bd0844296e6c815b6fe7
Vaggelis Athanasakis <athanasakis.evangelos@outlook.com>
2024-06-29 00:28:56 +0300

    Completed Whole Exercise 5

--- exercise2_5a/ex2_5.py
+++ exercise2_5a/ex2_5.py
@@ -1,10 +1,15 @@
 ## Vasilis Diakoloukas 
-
-
 import numpy as np
-from keras.datasets import mnist
-from keras.utils import np_utils
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+
+import sys
+import os
+
+# Change the stdout encoding to UTF-8
+#sys.stdout.reconfigure(encoding='utf-8')
 
 # Neural Network Dense (Fully Connected) Layer without activation
 class Dense():
@@ -16,7 +21,8 @@ class Dense():
     def forward(self, input):
         self.input = input
         # Add Code Here
-        fwd =  np.dot(self.input , self.weights) + self.bias
+        #fwd =  np.dot(self.input , self.weights) + self.bias
+        fwd =  np.dot(self.weights, self.input) + self.bias
         return fwd
 
     #Backward Propagation on a Dense Layer
@@ -25,18 +31,20 @@ class Dense():
     # dE_dB is dE/dB Gradient
     # dE_dX is dE/dX Gradient
     def backward(self, dE_dY, learning_rate):
-        dZ_dW = self.inputs.T                   # dZ_dW = Xi
-        dE_dW = np.dot(dE_dY,dZ_dW.T)           # dE_dW =  dE_dY / dZ_dW
+        dZ_dW = self.input.T                    # dZ_dW = Xi
+        dE_dW = np.dot(dE_dY,dZ_dW)/ self.input.shape[1]# dE_dW =  dE_dY / dZ_dW /// Normalizing by Batch Size so that the dot product can be calculated
+                                                                                ##// Ensures that the gradient computed is averaged over all samples in the batch.
         dY_dX = self.weights                    # dY_dX = W since we have no activation function
-        dE_dX = np.dot(dE_dY,dY_dX.T)           # dE_dX = dE_dY * dY_dX = dE_dY * W
+        dE_dX = np.dot(dY_dX.T,dE_dY)           # dE_dX = dE_dY * dY_dX = dE_dY * W
         
         # Since b is added to each neuron's output we need sum for each neuron across the batch
         # dE_dB = sum(dE_dZ * dZ_db) = sum(dE_dZ * 1) = sum(dE_dZ) = sum(dE_dY) => dE_dB = sum(dE_dY)
         # keepdims = True ensures that the resulting gradient has the same shape as B
-        dE_dB = np.sum(dE_dY, axis=1, keepdims=True)   
+        dE_dB = np.sum(dE_dY, axis=1, keepdims=True)/ self.input.shape[1]   
 
         self.update_weights(dE_dW, dE_dB, learning_rate)
         return dE_dX
+    
 
     # Update Layer Weights and bias
     def update_weights(self, dE_dW, dE_dB, learning_rate):
@@ -57,14 +65,12 @@ class Activation():
     # Y: is the output of the activation function
     def forward(self, input):
         self.input = input
-        # Add Code Here
-        Y = 
+        Y = self.activation(input)    # perform the forward pass
         return Y
 
     # Backward estimation of dE/dX using the activation prime (derivative)
-    def backward(self, dE_dY, learning_rate):
-        # Add Code Here
-        dE_dX =
+    def backward(self, dE_dY , learning_rate = 0): #learning rate is not used for this scenario 
+        dE_dX = dE_dY * self.activation_grad(self.input) 
         return dE_dX
  
 
@@ -76,9 +82,11 @@ class Softmax():
         self.output = tmp / np.sum(tmp)
         return self.output
 
-    def backward(self, output_gradient):
+    def backward(self, gradient_output,learning_rate = 0):
         n = np.size(self.output)
-        return np.dot((np.identity(n) - self.output.T) * self.output, output_gradient)
+        # J = np.diag(self.output.flatten()) - np.outer(self.output, self.output.T)
+        # return np.dot(J,gradient_output)
+        return np.dot((np.identity(n) - self.output.T) * self.output, gradient_output)
 
 
 # Tanh Activation Function
@@ -90,7 +98,7 @@ class Tanh(Activation):
 
         def tanh_grad(x):
             # Add Code Here
-            actGrad =
+            actGrad = 1 - np.tanh(x) ** 2
             return actGrad
 
         super().__init__(tanh, tanh_grad)
@@ -101,15 +109,13 @@ class Sigmoid(Activation):
     def __init__(self):
         # Logistic Activation Function
         def sigmoid(x):
-            # Add Code Here
-            act =
+            act = 1 / (1 + np.exp(-x))
             return act
 
         # Activation Function Gradient (Derivative)
         def sigmoid_grad(x):
-            # Add Code Here
-
-            actGrad =
+            s = sigmoid(x)
+            actGrad = s * (1 - s)
             return actGrad
 
         super().__init__(sigmoid, sigmoid_grad)
@@ -117,24 +123,26 @@ class Sigmoid(Activation):
         
 # Return the the cross entropy loss
 def loss_cross_entropy(y_true, y_pred):
-    # Add Code Here
-    loss =
+    B = len(y_true)
+    # Using the cross entropy formula to calculate the loss
+    # We have added a small value in every ln operation so that we avoid ln(0)
+    loss = (1/B)*np.sum(-y_true*np.log(y_pred+ 1e-15)-(1-y_true)*np.log(1-y_pred+1e-15))
     return loss
 
 # Return the derivative of the cross entropy loss
 def loss_cross_entropy_grad(y_true, y_pred):
-    # Add Code Here
-    lossGrad =
+    lossGrad =  -(y_true / y_pred - (1 - y_true) / (1 - y_pred))
     return lossGrad
 
 def mse(y_true, y_pred):
     # Add Code Here
-    loss =
+    loss = np.mean((y_true - y_pred) ** 2)
     return loss
 
 def mse_grad(y_true, y_pred):
     # Add Code Here
-    lossGrad =
+    n = y_true.shape[0]
+    lossGrad = (2 / n) * (y_pred - y_true)
     return lossGrad
 
 def preprocess_data(x, y, limit):
@@ -143,8 +151,9 @@ def preprocess_data(x, y, limit):
     x = x.astype("float32") / 255
     # encode output which is a number in range [0,9] into a vector of size 10
     # e.g. number 3 will become [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
-    y = np_utils.to_categorical(y)
+    y = to_categorical(y)
     y = y.reshape(y.shape[0], 10, 1)
+
     return x[:limit], y[:limit]
 
 def predict(network, input):
@@ -171,7 +180,7 @@ def read_next_batch(X, Y, batch_size, batch_idx=0):
     else:
         return None, None, None
 
-def train(network, x_train, y_train, epochs = 1000, learning_rate = 0.01, batch_size = 128, verbose = True):
+def train(network, x_train, y_train, epochs = 100, learning_rate = 0.01, batch_size = 128, verbose = True):
     num_batches = 0
     for epoch in range(epochs):
         epoch_loss = 0
@@ -202,6 +211,7 @@ def train(network, x_train, y_train, epochs = 1000, learning_rate = 0.01, batch_
             epoch_loss /= num_batches
         if verbose:
             print(f"{epoch + 1}/{epochs}, error={epoch_loss}")
+    print("\nEnd Of Training...")
 
 ##################################################################
 ###### Start Running the code from here
@@ -216,16 +226,41 @@ x_test, y_test = preprocess_data(x_test, y_test, 20)
 # Change the layer name and parameters appropriately to experiment with
 network = [
     Dense(28 * 28, 50),
+    #Tanh(),
     Sigmoid(),
     Dense(50, 10),
+    #Tanh()
     Softmax()
 ]
+
+# network = [
+#     Dense(28 * 28, 128),
+#     Sigmoid(),
+#     Dense(128, 64),
+#     Sigmoid(),
+#     Dense(64, 10),
+#     Softmax()
+# ]
+
+# network = [
+#     Dense(28 * 28, 256),
+#     Sigmoid(),
+#     Dense(256, 128),
+#     Sigmoid(),
+#     Dense(128, 64),
+#     Sigmoid(),
+#     Dense(64, 10),
+#     Softmax()
+# ]
+
+
 
 # train the network using the input data and stochastic Gradient Descent
 # Define different learning rates, epochs and batch_size to experiment with
 train(network, x_train, y_train, epochs=100, learning_rate=0.1, batch_size = 128)
 
 # Evaluate performance on test data
+print("\nStart Of Evaluation\n")
 for x, y in zip(x_test, y_test):
     output = predict(network, x)
     print('pred:', np.argmax(output), '\ttrue:', np.argmax(y))
